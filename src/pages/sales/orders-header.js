2d57582de9fe0854cf790be114ba1d4d4bf7dad4
Avdeev Denis <av-denis@yandex-team.ru>
2020-09-28 16:44:38 +0300

    Страница sales

---
+++ src/pages/sales/orders-header.js
@@ -0,0 +1,58 @@
+const header = [
+  {
+    id: 'id',
+    title: 'ID',
+    sortable: true,
+    sortType: 'number'
+  },
+  {
+    id: 'user',
+    title: 'Клиент',
+    sortable: true,
+    sortType: 'string'
+  },
+  {
+    id: 'createdAt',
+    title: 'Дата',
+    sortable: true,
+    sortType: 'custom',
+    customSorting: ({ createdAt: ACreatedAt }, { createdAt: BCreatedAt }) => {
+      return Number(new Date(ACreatedAt)) > Number(new Date(BCreatedAt)) ? 1 : -1;
+    },
+    template: data => {
+      const date = new Date(data);
+      const dateWithTimezone = Number(date) + date.getTimezoneOffset() * 60 * 1000;
+
+      return `
+          <div class="sortable-table__cell">
+            ${new Date(dateWithTimezone).toLocaleString('ru', {
+              month: 'long',
+              day: '2-digit',
+              year: 'numeric'
+            })}
+          </div>
+        `;
+    }
+  },
+  {
+    id: 'totalCost',
+    title: 'Стоимость',
+    sortable: true,
+    sortType: 'number',
+    template: data => {
+      return `
+        <div class="sortable-table__cell">
+          $${data}
+        </div>
+      `;
+    }
+  },
+  {
+    id: 'delivery',
+    title: 'Статус',
+    sortable: true,
+    sortType: 'string'
+  },
+];
+
+export default header;
